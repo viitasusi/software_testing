1f4ad2a2c07bf596f5798b0f6522068fc213c4e9
verkasree <verkasree@gmail.com>
2024-11-21 10:03:47 +0200

    Added Coveralls workflow

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ export default {
     '^.+\\.js$': 'esbuild-jest', // Use esbuild for transforming JavaScript
   },
   testEnvironment: 'node', // Make sure to use Node environment for testing
+  collectCoverage: true, // Ensure coverage is enabled
+  coverageReporters: ['lcov', 'text'],
 };
