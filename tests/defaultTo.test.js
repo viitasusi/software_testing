c0f21c8c960b78ec2e2b94a1dc4bcc09469d1101
verkasree <verkasree@gmail.com>
2024-12-04 15:59:01 +0200

    Additional tests for defaultTo.js

--- tests/defaultTo.test.js
+++ tests/defaultTo.test.js
@@ -5,7 +5,37 @@ describe('defaultTo', () => {
     expect(defaultTo(1, 10)).toBe(1);
   });
 
+  test('returns given object', () => {
+    const obj = { name: 'Thomas' };
+    expect(defaultTo(obj, { name: 'Peter' })).toEqual(obj);
+  });
+
   test('returns default value instead of undefined', () => {
     expect(defaultTo(undefined, 10)).toBe(10);
   });
+
+  test('returns default value instead of null', () => {
+    expect(defaultTo(null, 10)).toBe(10);
+  });
+
+  test('returns default value instead of NaN', () => {
+    expect(defaultTo(NaN, 10)).toBe(10);
+  });
+
+  test('returns given false value instead of default', () => {
+    expect(defaultTo(false, true)).toBe(false);
+  });
+
+  test('returns given 0 value instead of default', () => {
+    expect(defaultTo(0, 10)).toBe(0);
+  });
+
+  test('returns given empty string instead of default', () => {
+    expect(defaultTo('', 'Hi')).toBe('');
+  });
+
+  test('returns given empty object', () => {
+    expect(defaultTo({}, { name: 'Peter' })).toEqual({});
+  });
+
 });
